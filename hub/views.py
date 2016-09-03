6fa8dacea81df147cb64356c068dca931e95ef0a
Chinseok Lee <allieuslee@gmail.com>
2016-09-03 10:30:53 +0900

    hub:post_detail 뷰 구현하고, url reverse 로 링크 추가

--- hub/views.py
+++ hub/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'hub/index.html')
 
+def post_detail(request, pk):
+    return render(request, 'hub/post_detail.html')
+
