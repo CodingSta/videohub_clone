f153aa648e9ea24bc6d45fc527f961e519bf8282
Chinseok Lee <allieuslee@gmail.com>
2016-09-03 11:24:50 +0900

    hub/models.py 생성 및 마이그레이션 파일 생성

--- hub/models.py
+++ hub/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    content = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
