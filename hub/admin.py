ba29040916e1ca4a957908864e15ee31237d171e
Chinseok Lee <allieuslee@gmail.com>
2016-09-03 11:46:03 +0900

    hub Post/Tag 모델에 대해 admin 적용

--- hub/admin.py
+++ hub/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content_length')
+
+    def content_length(self, post):
+        return '{} 글자'.format(len(post.content))
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
--- hub/models.py
+++ hub/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
@@ -8,7 +10,14 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
 
+
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
 
