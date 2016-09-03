9999d0841a87814f4ef60c35c4680072b7eae187
Chinseok Lee <allieuslee@gmail.com>
2016-09-03 10:22:56 +0900

    root 주소로 접속시에 /hub/ 주소로 강제 이동

--- videohub_clone/urls.py
+++ videohub_clone/urls.py
@@ -13,10 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
+def redirect_to_hub(request):
+    return redirect('hub:index')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hub/', include('hub.urls', namespace='hub')),
+
+    # Root 로 접속하면, 지정 뷰로 이동
+    # 1) lambda 함수로 뷰 지정
+    # url(r'^$', lambda request: redirect('hub:index')),
+    # 2) 함수로 뷰 지정
+    # url(r'^$', redirect_to_hub),
+    # 3) 클래스 기반 뷰 (Class Based View) 로 지정
+    url(r'^$', RedirectView.as_view(pattern_name='hub:index')),
 ]
